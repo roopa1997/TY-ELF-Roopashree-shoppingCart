7269b16dbdbc4452ee55945e2b6e2329610d267e
roopa1997 <cvroopa1997@gmail.com>
2019-12-30 09:36:11 +0530

    shopping cart

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <>
       <Router>
         <nav class="navbar navbar-expand-lg navbar-dark bg-info">
-        <Link to='/' class='navbar-brand'>Shopping</Link>
+        <Link to='/' class='navbar-brand'>Shop Now</Link>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
